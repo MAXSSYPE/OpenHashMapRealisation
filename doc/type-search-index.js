8919f9977c3057c061ba80ff9a0c132af134beab
Maksym <dudkamv@gmail.com>
2020-06-10 00:15:15 +0300

    added javadocs

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.company.project","l":"OpenHashMap"},{"p":"com.company.project","l":"OpenHashMapImpl"},{"p":"<Unnamed>","l":"OpenHashMapTest"},{"p":"com.company.project","l":"Pair"}]
